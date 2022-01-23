ca43003460f4d39a07d19a23787d671b9abdf2eb
ObjectPlayer <objectplayer@gmail.com>
2022-01-23 21:04:59 +0500

    test-cases written for all methods

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,3 +1,4 @@
+import EosioTokenContract from "../BlockchainServices/contracts/EosioTokenContract";
 import EosDefaultMethodService from "../BlockchainServices/defaults/EosService";
 
 jest.setTimeout(1000000);
@@ -10,5 +11,34 @@ describe("Testing Wax Blockchain Service", () => {
         let res = await blockchainService.getAccountInfo(accountName);
         expect(res).not.toBe(null);
     })
+
+    it('Get User Balance', async () => {
+        const blockchainService = new EosDefaultMethodService();
+        const accountName = "objectplayer"
+        let res = await blockchainService.getUserBalance(accountName);
+        expect(res).not.toBe(null);
+    })
+
+    it('Get Transaction Details', async () => {
+        const blockchainService = new EosDefaultMethodService();
+        const trxId = "74d4757412b973c1427ec6605ab7583b527c6482c3f6d64dcf59b9f9c752212f"
+        let res = await blockchainService.getTransactionDetails(trxId);
+        expect(res).not.toBe(null);
+    })
+
+    it('Transfer token', async () => {
+        const blockchainService = new EosDefaultMethodService();
+        const eosioTokenContract = new EosioTokenContract();
+
+        const from = "objectplayer";
+        const to = "shamgoswamii";
+        const quantity = "1.00000000 WAX";
+        const memo = "testing"
+
+        const transferAction = eosioTokenContract.eosioTokenContract(from,to,quantity,memo)        
+        let res = await blockchainService.defaultPushAction(transferAction);
+        expect(res).not.toBe(null);
+    })
+
 
 })
