a0f32e3aff108ae4ac25f16aa4c6f47083eb17ac
ObjectPlayer <objectplayer@gmail.com>
2022-01-25 23:23:10 +0500

    constant values added

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,5 +1,6 @@
 import EosioTokenContract from "../BlockchainServices/contracts/EosioTokenContract";
 import EosDefaultMethodService from "../BlockchainServices/defaults/EosService";
+import { ACCOUNT_NAME_1, ACCOUNT_NAME_2, AMOUNT, MEMO, TRANSACTION_ID } from "../BlockchainServices/constants"
 
 jest.setTimeout(1000000);
 
@@ -7,21 +8,21 @@ describe("Testing Wax Blockchain Service", () => {
 
     it('Get Account Info', async () => {
         const blockchainService = new EosDefaultMethodService();
-        const accountName = "objectplayer"
+        const accountName = ACCOUNT_NAME_1
         let res = await blockchainService.getAccountInfo(accountName);
         expect(res).not.toBe(null);
     })
 
     it('Get User Balance', async () => {
         const blockchainService = new EosDefaultMethodService();
-        const accountName = "objectplayer"
+        const accountName = ACCOUNT_NAME_1
         let res = await blockchainService.getUserBalance(accountName);
         expect(res).not.toBe(null);
     })
 
     it('Get Transaction Details', async () => {
         const blockchainService = new EosDefaultMethodService();
-        const trxId = "74d4757412b973c1427ec6605ab7583b527c6482c3f6d64dcf59b9f9c752212f"
+        const trxId = TRANSACTION_ID
         let res = await blockchainService.getTransactionDetails(trxId);
         expect(res).not.toBe(null);
     })
@@ -30,12 +31,12 @@ describe("Testing Wax Blockchain Service", () => {
         const blockchainService = new EosDefaultMethodService();
         const eosioTokenContract = new EosioTokenContract();
 
-        const from = "objectplayer";
-        const to = "shamgoswamii";
-        const quantity = "1.00000000 WAX";
-        const memo = "testing"
+        const from = ACCOUNT_NAME_1;
+        const to = ACCOUNT_NAME_2;
+        const quantity = AMOUNT;
+        const memo = MEMO
 
-        const transferAction = eosioTokenContract.eosioTokenContract(from,to,quantity,memo)        
+        const transferAction = eosioTokenContract.eosioTokenContract(from, to, quantity, memo)
         let res = await blockchainService.defaultPushAction(transferAction);
         expect(res).not.toBe(null);
     })
