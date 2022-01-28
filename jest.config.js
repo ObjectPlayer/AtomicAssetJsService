8ba1f7c807c6f3b2f267f18f415bc18010cae955
ObjectPlayer <objectplayer@gmail.com>
2022-01-28 23:28:18 +0500

    .env working in jest file

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     roots: ['<rootDir>/tests'],
+    setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
     testMatch: [
         '**/__tests__/**/*.+(ts|tsx|js)',
         '**/?(*.)+(spec|test).+(ts|tsx|js)',
